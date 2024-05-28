43b552e338d2982256d1f09abcb4218baf1e31bd
Tanachock <tanachock.saelo@gmail.com>
2024-05-28 17:27:06 +0700

    fix api service

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -7,12 +7,27 @@ import { catchError, throwError } from 'rxjs';
 })
 export class ApiService {
 
-  private apiUrl = 'https://api.myanimelist.net/v2/anime/season/2017/summer?limit=4';
-
   constructor(private http: HttpClient) { }
 
   selectSeason(year:string,season: string){
 
-    return this.http.get(`https://api.jikan.moe/v4/seasons/${year}/${season}`)
+    return this.http.get(`https://api.jikan.moe/v4/seasons/${year}/${season}`).pipe(
+      catchError(this.handleError)  // Catch errors
+    );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    let errorMessage = '';
+
+    if (error.error instanceof ErrorEvent) {
+      // Client-side error
+      errorMessage = `An error occurred: ${error.error.message}`;
+    } else {
+      // Server-side error
+      errorMessage = `Server returned code: ${error.status}, error message is: ${error.message}`;
+    }
+
+    console.error(errorMessage);
+    return throwError(() => new Error(errorMessage));
   }
 }
